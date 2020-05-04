6dff77afcd7f36cd3cd2d41b14a76d2540b83dae
Andrei Borza <44921296+BorzaAndrei@users.noreply.github.com>
2020-05-04 17:51:50 +0300

    Inventory working
    Adds inventory system and ability to pick up items and use them on characters.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,9 @@ public class GameManager : MonoBehaviour
     public bool dialogActive;
     public bool fadingBetweenAreas;
 
+    public string[] itemsHeld;
+    public int[] noOfItems;
+    public Item[] referenceItems;
 
     public CharStats[] playerStats;
 
@@ -20,6 +23,8 @@ public class GameManager : MonoBehaviour
         instance = this;
 
         DontDestroyOnLoad(gameObject);
+
+        SortItems();
     }
 
     // Update is called once per frame
@@ -32,6 +37,120 @@ public class GameManager : MonoBehaviour
         else
         {
             PlayerController.instance.canMove = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            AddItem("Iron Armour");
+            AddItem("nu");
+
+            RemoveItem("Health Potion");
+        }
+    }
+
+    public Item GetItemDetails(string itemName)
+    {
+        for (int i = 0; i < referenceItems.Length; i++)
+        {
+            if (referenceItems[i].itemName == itemName)
+            {
+                return referenceItems[i];
+            }
+        }
+
+        return null;
+    }
+
+    public void SortItems()
+    {
+        bool moreItems = true;
+        for (int i = 0; i < itemsHeld.Length - 1; i++)
+        {
+            int j = i + 1;
+            while (itemsHeld[i] == "" && moreItems)
+            {
+                itemsHeld[i] = itemsHeld[j];
+                itemsHeld[j] = "";
+
+                noOfItems[i] = noOfItems[j];
+                noOfItems[j] = 0;
+                j++;
+
+                if (j >= itemsHeld.Length)
+                {
+                    moreItems = false;
+                }
+            }
+        }
+    }
+
+    public void AddItem(string itemToAdd)
+    {
+        int position = 0;
+        bool foundSpace = false;
+
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            if (itemsHeld[i] == "" || itemsHeld[i] == itemToAdd)
+            {
+                position = i;
+                foundSpace = true;
+                break;
+            }
+        }
+
+        if (foundSpace)
+        {
+            bool itemExists = false;
+            for (int i = 0; i < referenceItems.Length; i++)
+            {
+                if (referenceItems[i].itemName == itemToAdd)
+                {
+                    itemExists = true;
+                    break;
+                }
+            }
+            if (itemExists)
+            {
+                itemsHeld[position] = itemToAdd;
+                noOfItems[position]++;
+            }
+            else
+            {
+                Debug.LogError(itemToAdd + " does NOT exist!");
+            }
+        }
+
+        GameMenu.instance.ShowItems();
+    }
+
+    public void RemoveItem(string itemToRemove)
+    {
+        bool foundItem = false;
+        int position = 0;
+
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            if (itemsHeld[i] == itemToRemove)
+            {
+                foundItem = true;
+                position = i;
+                break;
+            }
+        }
+
+        if (foundItem)
+        {
+            noOfItems[position]--;
+            if (noOfItems[position] <= 0)
+            {
+                itemsHeld[position] = "";
+            }
+            GameMenu.instance.ShowItems();
+        }
+        else
+        {
+            Debug.LogError("Couldn't find " + itemToRemove);
         }
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item Type")]
+    public bool isItem;
+    public bool isWeapon;
+    public bool isArmour;
+
+    [Header("Basic Details")]
+    public string itemName;
+    public string description;
+    public int value;
+    public Sprite itemSprite;
+
+    [Header("Item Details")]
+    public int amountToChange;
+    public bool affectHp, affectMp, affectStr;
+
+    [Header("Weapon Details")]
+    public int wpnStr;
+
+    [Header("Armour Details")]
+    public int armourStr;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Use(int charToUseOn)
+    {
+        CharStats selectedChar = GameManager.instance.playerStats[charToUseOn];
+        if (isItem)
+        {
+            if (affectHp)
+            {
+                selectedChar.currentHp += amountToChange;
+
+                if (selectedChar.currentHp > selectedChar.maxHp)
+                {
+                    selectedChar.currentHp = selectedChar.maxHp;
+                }
+            }
+            if (affectMp)
+            {
+                selectedChar.currentMp += amountToChange;
+
+                if (selectedChar.currentMp > selectedChar.maxMp)
+                {
+                    selectedChar.currentMp = selectedChar.maxMp;
+                }
+            }
+            if (affectStr)
+            {
+                selectedChar.strenght += amountToChange;
+            }
+        }
+
+        if (isWeapon)
+        {
+            if (selectedChar.equippedWeapon != "")
+            {
+                GameManager.instance.AddItem(selectedChar.equippedWeapon);
+            }
+            selectedChar.equippedWeapon = itemName;
+            selectedChar.weaponPower = wpnStr;
+        }
+
+        if (isArmour)
+        {
+            if (selectedChar.equippedArmour != "")
+            {
+                GameManager.instance.AddItem(selectedChar.equippedArmour);
+            }
+            selectedChar.equippedArmour = itemName;
+            selectedChar.armourPower = armourStr;
+        }
+
+        GameManager.instance.RemoveItem(itemName);
+    }
+}
