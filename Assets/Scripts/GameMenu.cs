c24683589b102b0b67cfcf9e72c30a16e881041c
Andrei Borza <44921296+BorzaAndrei@users.noreply.github.com>
2020-05-05 11:08:53 +0300

    Audio system working
    Implements audio system that is capable of playing music from scene to scene and simple SFX sounds.

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -60,6 +60,7 @@ public class GameMenu : MonoBehaviour
                 UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
             }
+            AudioManager.instance.PlaySFX(5);
         }
     }
 
@@ -226,5 +227,10 @@ public class GameMenu : MonoBehaviour
     {
         GameManager.instance.SaveData();
         QuestManager.instance.SaveQuestData();
+    }
+
+    public void PlayButtonSound()
+    {
+        AudioManager.instance.PlaySFX(4);
     }
 }
